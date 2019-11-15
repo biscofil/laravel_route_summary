05fe97b185baf41dfd9ae67ee4611ec1a374ae16
Filippo <filippo.bisconcin@gmail.com>
2019-11-15 17:06:24 +0100

    actual code

--- src/Commands/GetRouteSummary.php
+++ src/Commands/GetRouteSummary.php
@@ -5,17 +5,88 @@ namespace Biscofil\LaravelRouteSummary\Commands;
 class GetRouteSummary extends Command
 {
 
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'route:summary';
 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'Command description';
 
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         parent::__construct();
     }
 
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
     public function handle()
     {
-        dd("OOOOK");
+
+        $app = app();
+        $routes = $app->routes->getRoutes();
+
+        $out = array_map(function (Route $route) {
+
+            //middleware
+            $middleware = $route->gatherMiddleware();
+            $middleware = array_map(function ($middleware) {
+
+                if (strpos($middleware, ':') !== false) {
+                    //parameters
+
+                    $middlewareParts = preg_split('/\:/', $middleware);
+
+                    $middlewareName = array_shift($middlewareParts);
+
+                    $params = preg_split('/\,/', $middlewareParts[0]);
+
+                    return [
+                        'middleware' => $middlewareName,
+                        'params' => $params
+                    ];
+                } else {
+                    return $middleware;
+                }
+            }, $middleware);
+
+            $controllerParts = preg_split('/\@/', $route->getAction()['controller']);
+
+            $controllerName = $controllerParts[0];
+            $controllerMethod = $controllerParts[1];
+
+
+            $parameters = array_merge(array_map(function ($parameterName) use ($controllerMethod, $controllerName) {
+                $p = new ReflectionParameter([$controllerName, $controllerMethod], $parameterName);
+                return [
+                    $parameterName => $p->getType()
+                ];
+            }, $route->parameterNames()));
+
+            return [
+                'uri' => $route->uri,
+                'controller' => $controllerName,
+                'controller_method' => $controllerMethod,
+                'parameters' => $parameters, //$route->getCompiled()->getPathVariables(),
+                'methods' => $route->methods(),
+                'middleware' => $middleware
+            ];
+        }, $routes);
+
+        dd($out);
     }
 }
